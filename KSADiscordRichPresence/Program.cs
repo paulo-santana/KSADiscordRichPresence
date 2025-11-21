3a6224797a908fdf6196a3584d5e8135c93c698d
paulo <paulo.santana.r@gmail.com>
2025-11-20 23:05:55 -0300

    feat: implement first presence setup

---
+++ KSADiscordRichPresence/Program.cs
@@ -0,0 +1,19 @@
+﻿using StarMap.API;
+using KSA;
+
+namespace KSADiscordRichPresence
+{
+	[StarMapMod]
+	public class KSADiscordRichPresence
+	{
+		private DiscordRpcClientManager? DiscordRpcClientManager { get; set; }
+
+		[StarMapImmediateLoad]
+		public void Init(Mod definingMod)
+		{
+			DiscordRpcClientManager = new DiscordRpcClientManager();
+			DiscordRpcClientManager?.Setup();
+			DiscordRpcClientManager?.SetPresence("Sailing the Stars");
+		}
+	}
+};
